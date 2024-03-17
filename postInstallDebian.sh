e58a7de18cd9b925e8cb4a07ed73647d7b17ff9b
Valentin Cuzin-Rambaud <valentincuzin@murena.io>
2024-03-17 15:29:06 +0100

    new script

---
+++ postInstallDebian.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# désinstaller bloatware
+# update puis installer backport et flatpack puis upgrade
+# installer htop et démo htop
+# installer vscode et extensions
+# chromium intelliJ postman java 21, nodejs, npm, dBeaver
+# 
+#
+# reboot
